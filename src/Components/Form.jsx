0ce6e0824179f27949cb1962d0dea929db6327ea
Guilherme <guiteixeira1926@gmail.com>
2024-05-03 11:50:54 -0400

    feat: add handeSubmit function to create a new item element when the form is submited

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -4,8 +4,20 @@ export default function Form() {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip ?</h3>
       <select
         value={quantity}
