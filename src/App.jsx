8f94b312c3b394cd08ffdcf63f1ee03331d2b375
Guilherme <guiteixeira1926@gmail.com>
2024-05-03 11:43:33 -0400

    feat: add the select, input and button element to the form

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Form from "./Components/Form";
 import Logo from "./Components/Logo";
 
 export default function App() {
   return (
     <div className="app">
       <Logo />
+      <Form />
     </div>
   );
 }
