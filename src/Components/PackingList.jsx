e2dda28da807fced846839d0c3c79bfe775d1685
Guilherme <guiteixeira1926@gmail.com>
2024-05-03 12:18:06 -0400

    fix: fix the html structure of the packing list component

--- src/Components/PackingList.jsx
+++ src/Components/PackingList.jsx
@@ -4,15 +4,17 @@ import Item from "./Item";
 
 export default function PackingList({ items, onDeleteItem, onToggleItem }) {
   return (
-    <ul className="list">
-      {items.map((item) => (
-        <Item
-          item={item}
-          onDeleteItem={onDeleteItem}
-          onToggleItem={onToggleItem}
-          key={item.id}
-        />
-      ))}
-    </ul>
+    <div className="list">
+      <ul>
+        {items.map((item) => (
+          <Item
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+            key={item.id}
+          />
+        ))}
+      </ul>
+    </div>
   );
 }
